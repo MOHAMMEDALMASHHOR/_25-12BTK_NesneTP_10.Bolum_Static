4dcb92cc35bebfea0eca84e1b146c178f32d0161
MOHAMMEDALMASHHOR <146783493+MOHAMMEDALMASHHOR@users.noreply.github.com>
2023-12-25 19:38:08 +0300

    Add files via upload

---
+++ _25-12BTK_NesneTP_10.Bolum_Static/BookStore/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookStore
+{
+    public class Book
+    {
+        public int BookId
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Title
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public decimal Price
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int CategoryId
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Category Category
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public System.Collections.Generic.ICollection<BookAuthor> BookAuthors
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public BookDetail BookDetail
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
